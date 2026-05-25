640a5e1451bd447a487e9592c9d6546ed2585183
Manuel <andrianarivo24@gmail.com>
2026-05-25 16:07:55 +0300

    chore: initial commit

---
+++ src/test/java/com/rbacsystem/RbacSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbacsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
